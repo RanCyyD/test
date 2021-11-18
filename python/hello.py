0db5a77be04bf20c2c16bb38594642af443b3249
RanCyyD <alexander.jann@gmx.de>
2021-11-18 17:09:48 +0100

    aenderung

--- python/hello.py
+++ python/hello.py
@@ -10,3 +10,8 @@ print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1)+ ' in a year!')
 
+print('What is your age?')
+myAge2 = input()
+print('Together your are ' + str(int(myAge) + int(myAge2))+ ' !')
+
+print(myAge + myAge2)
