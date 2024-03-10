73bc225ecfce71fb698cb9501b44a0f31712ff2f
RanCyyD <alexander.jann@gmx.de>
2024-03-10 12:40:49 +0100

    new game

--- python/hello.py
+++ python/hello.py
@@ -1,17 +1,18 @@
 # This programm says hello and asks for my name
 
-print('Hello world!')
-print ('What is your name?') # asks for name
+print("Hello world!")
+print("Mal schauen")
+print("What is your name?")  # asks for name
 myName = input()
-print('It is good to meet you, ' +myName) 
-print('The length of your name is:')
+print("It is good to meet you, " + myName)
+print("The length of your name is:")
 print(len(myName))
-print('What is your age?')
+print("What is your age?")
 myAge = input()
-print('You will be ' + str(int(myAge) + 1)+ ' in a year!')
+print("You will be " + str(int(myAge) + 1) + " in a year!")
 
-print('What is your age?')
+print("What is your age?")
 myAge2 = input()
-print('Together your are ' + str(int(myAge) + int(myAge2))+ ' !')
+print("Together your are " + str(int(myAge) + int(myAge2)) + " !")
 
-print(myAge + myAge2)
+# print(myAge + myAge2)
